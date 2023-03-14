0576b8ebd46254cbaa4a28d222b60cfed4981140
prathapanikalyani <prathapanikalyani@gmail.com>
2023-03-14 13:53:52 +0530

    This is currency converter initial commit

---
+++ currency-conversion-service/src/main/java/com/aspire/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,19 @@
+package com.aspire.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.aspire.microservices.currencyconversionservice.model.CurrencyConversion;
+
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to);
+
+}
